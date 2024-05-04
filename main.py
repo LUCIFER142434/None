0b8b1f072d48119c4990085786c15122da117de7
unknown <bexruzraxmanov59@gmail.com>
2024-05-04 19:19:16 +0500

    zdachi

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 print("Hello world")
 
-tst = 5
-if tst > 10:
-    print(True)
-else:
-    print(False)
-print("text")
+# tst = 5
+# if tst > 10:
+#     print(True)
+# else:
+#     print(False)
+
+#1
+# tst = 15 
+# if tst > 10 and tst < 20:
+#     print('YES')
+# else:
+#     print('NOT')
+#2
+# tst = -5
+# if tst < 0 and tst > -10:
+#     print('YES')
+# else:
+#     print('NOT')
+#3
+# tst = ['a','b','c','d','e']
+# if len(tst) < 6:
+#     print('YES')
+# else:
+#     print('NOT')
